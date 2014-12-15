569848c6422c271612768d7161e7d5c466065bdb
Michael Diodone <michael.diodone@devbliss.com>
2014-12-15 15:44:59 +0100

    Extended our plugin from the fomerly used plugin to ease development

--- src/main/groovy/com/devbliss/docker/DockerPlugin.groovy
+++ src/main/groovy/com/devbliss/docker/DockerPlugin.groovy
@@ -4,12 +4,10 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class DockerPlugin implements Plugin<Project> {
-	
+
   @Override
   public void apply(Project project) {
-      project.task('hello') << {
-          println "Hello from the DockerPlugin"
-      }
+    project.getPlugins().apply('de.gesellix.docker')
   }
 }
 
