65490ffd0985a34f31921016d3bc70a8dd289d34
Christian Soth <christian.soth@devbliss.com>
2015-01-07 10:47:56 +0100

    Add Junit for custom gradle plugin

---
+++ src/test/groovy/com/devbliss/docker/DockerPluginTest.groovy
@@ -0,0 +1,17 @@
+package com.devbliss.docker
+
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Test
+
+/**
+ * Created by Christian Soth <christian.soth@devbliss.com> on 07.01.15.
+ */
+
+class DockerPluginTest extends GroovyTestCase{
+
+  @Test
+  public void test() {
+    Project project = ProjectBuilder.builder().build()
+  }
+}
