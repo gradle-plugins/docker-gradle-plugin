e8c4ae19ee24e3f9f63cba00d69b2a5f17fcd9fe
Christian Soth <christian.soth@devbliss.com>
2015-02-18 12:16:42 +0100

    Remove TaskAction from buildAndPushDockerImageTask

--- src/main/groovy/com/devbliss/docker/task/BuildAndPushDockerImageTask.groovy
+++ src/main/groovy/com/devbliss/docker/task/BuildAndPushDockerImageTask.groovy
@@ -1,23 +1,11 @@
 package com.devbliss.docker.task
 
 import de.gesellix.gradle.docker.tasks.AbstractDockerTask
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.Optional
-import org.gradle.api.tasks.TaskAction
 
 class BuildAndPushDockerImageTask extends AbstractDockerTask {
 
     BuildAndPushDockerImageTask() {
-        description = "Build and push your images and start depending containers for this project"
+        description = "Build and push your images for this project"
         group = "Devbliss"
-    }
-
-    @Input
-    @Optional
-    def buildContextDirectory
-
-    @TaskAction
-    public void run() {
-
     }
 }
