519569fe6d860e0c5d79d5c7eb0d1a2f7ae2bfdc
Christian Soth <christian.soth@devbliss.com>
2015-02-18 15:14:53 +0100

    Refactor GetServiceDepencenciesTask and use Wrapper class

--- src/main/groovy/com/devbliss/docker/task/BuildAndPushDockerImageTask.groovy
+++ src/main/groovy/com/devbliss/docker/task/BuildAndPushDockerImageTask.groovy
@@ -2,10 +2,10 @@ package com.devbliss.docker.task
 
 import de.gesellix.gradle.docker.tasks.AbstractDockerTask
 
-class BuildAndPushDockerImageTask extends AbstractDockerTask {
+class BuildAndPushDockerImageTask extends AbstractDockerClusterTask {
 
     BuildAndPushDockerImageTask() {
+        super()
         description = "Build and push your images for this project"
-        group = "Devbliss"
     }
 }
